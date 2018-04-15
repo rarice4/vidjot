20e0fa8c6948c46e849dc2427997f969828f3e4c
Robert Rice <rarice@g.cofc.edu>
2018-04-15 15:29:42 -0600

    dd handlebars and create home and about views

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+//handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+//index route
+app.get('/', (req,res)=>{
+  res.render('index')
+});
+
+app.get('/about',(req,res)=>{
+  res.render("about")
+})
+
 
 const port = 5000;
 
