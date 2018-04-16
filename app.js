5ce055d203b77e4142c1011f5925e29b0dc18206
Robert Rice <rarice@g.cofc.edu>
2018-04-15 19:14:07 -0600

    idea model

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// map global promise - get rid of warning
+//mongoose.Promise = global.Promise;
+
+//connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev').then(()=>{
+  console.log("mongodb connected!!!");
+}).catch((err)=>{
+  console.log(err);
+});
+
+//load idea model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
 //handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
